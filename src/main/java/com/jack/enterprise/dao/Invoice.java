ca2d8ffaa99cd176e60125cd1c18de7282bfeb05
EchoDSun <echodsun@163.com>
2019-05-28 21:51:40 +0800

    get and set

--- src/main/java/com/jack/enterprise/dao/Invoice.java
+++ src/main/java/com/jack/enterprise/dao/Invoice.java
@@ -35,5 +35,214 @@ public class Invoice {
 	//其他信息
 	private String createPersonId;  //创建者id
 	private Date createTime;  //创建时间，自动生成
+	
+	
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the number
+	 */
+	public String getNumber() {
+		return number;
+	}
+	/**
+	 * @param number the number to set
+	 */
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	/**
+	 * @return the lian
+	 */
+	public String getLian() {
+		return lian;
+	}
+	/**
+	 * @param lian the lian to set
+	 */
+	public void setLian(String lian) {
+		this.lian = lian;
+	}
+	/**
+	 * @return the operatingTime
+	 */
+	public Date getOperatingTime() {
+		return operatingTime;
+	}
+	/**
+	 * @param operatingTime the operatingTime to set
+	 */
+	public void setOperatingTime(Date operatingTime) {
+		this.operatingTime = operatingTime;
+	}
+	/**
+	 * @return the outPerson
+	 */
+	public String getOutPerson() {
+		return outPerson;
+	}
+	/**
+	 * @param outPerson the outPerson to set
+	 */
+	public void setOutPerson(String outPerson) {
+		this.outPerson = outPerson;
+	}
+	/**
+	 * @return the signPerson
+	 */
+	public String getSignPerson() {
+		return signPerson;
+	}
+	/**
+	 * @param signPerson the signPerson to set
+	 */
+	public void setSignPerson(String signPerson) {
+		this.signPerson = signPerson;
+	}
+	/**
+	 * @return the accountant1
+	 */
+	public String getAccountant1() {
+		return accountant1;
+	}
+	/**
+	 * @param accountant1 the accountant1 to set
+	 */
+	public void setAccountant1(String accountant1) {
+		this.accountant1 = accountant1;
+	}
+	/**
+	 * @return the accountant2
+	 */
+	public String getAccountant2() {
+		return accountant2;
+	}
+	/**
+	 * @param accountant2 the accountant2 to set
+	 */
+	public void setAccountant2(String accountant2) {
+		this.accountant2 = accountant2;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the unit
+	 */
+	public String getUnit() {
+		return unit;
+	}
+	/**
+	 * @param unit the unit to set
+	 */
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	/**
+	 * @return the count
+	 */
+	public int getCount() {
+		return count;
+	}
+	/**
+	 * @param count the count to set
+	 */
+	public void setCount(int count) {
+		this.count = count;
+	}
+	/**
+	 * @return the unitPrice
+	 */
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+	/**
+	 * @param unitPrice the unitPrice to set
+	 */
+	public void setUnitPrice(int unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	/**
+	 * @return the privce
+	 */
+	public int getPrivce() {
+		return privce;
+	}
+	/**
+	 * @param privce the privce to set
+	 */
+	public void setPrivce(int privce) {
+		this.privce = privce;
+	}
+	/**
+	 * @return the remarks
+	 */
+	public String getRemarks() {
+		return remarks;
+	}
+	/**
+	 * @param remarks the remarks to set
+	 */
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	/**
+	 * @return the createPersonId
+	 */
+	public String getCreatePersonId() {
+		return createPersonId;
+	}
+	/**
+	 * @param createPersonId the createPersonId to set
+	 */
+	public void setCreatePersonId(String createPersonId) {
+		this.createPersonId = createPersonId;
+	}
+	/**
+	 * @return the createTime
+	 */
+	public Date getCreateTime() {
+		return createTime;
+	}
+	/**
+	 * @param createTime the createTime to set
+	 */
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	
+	
+	
 
 }
